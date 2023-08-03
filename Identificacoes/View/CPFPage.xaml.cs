e77cbeb6722e8311fe61e5813db6510d61690c75
Éden Alencar <eden.alencar@outlook.com>
2023-08-02 21:00:08 -0300

    Implementação do CPF

--- Identificacoes/View/CPFPage.xaml.cs
+++ Identificacoes/View/CPFPage.xaml.cs
@@ -1,5 +1,9 @@
+using Identificacoes.Bu;
+using Identificacoes.Util;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using System.Text;
+using Windows.ApplicationModel.DataTransfer;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -18,7 +22,28 @@ namespace Identificacoes.View
 
         private void Gerar_CPF(object sender, RoutedEventArgs e)
         {
-
+            var builder = new StringBuilder();
+            for (int i = 0; i < quantidade.Value; i++)
+            {
+                var identificacaoCPF = IdentificacaoFactory.GetInstance().GetIdentificacao(Constantes.CPF);
+                if ((bool)formatado.IsChecked)
+                {
+                    builder.Append(identificacaoCPF.ObterIdentificacaoFormatada()+"\n");
+                }
+                else
+                {
+                    builder.Append(identificacaoCPF.GetIdentificacao() + "\n");
+                }
+            }
+            resultado.Text = builder.ToString();
         }
+
+        private void Copiar_Resultado(object sender, RoutedEventArgs e)
+        {
+            DataPackage package = new DataPackage();
+            package.SetText(resultado.Text);
+            Clipboard.SetContent(package);
+        }
+        
     }
 }
